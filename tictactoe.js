2ea4f38a4de22763af5a4fb745810655baf63afb
shreyasborole <shreyas.borole2000@gmail.com>
2022-04-29 17:59:09 +0530

    Refactored P v AI

--- tictactoe.js
+++ tictactoe.js
@@ -1,10 +1,8 @@
 // Javascript program to find the
 // next optimal move for a player
-class Move
-{
-	constructor()
-	{
-		let row,col;
+class Move {
+	constructor() {
+		let row, col;
 	}
 }
 
@@ -13,39 +11,33 @@ let player = 'o', opponent = 'x';
 // This function returns true if there are moves
 // remaining on the board. It returns false if
 // there are no moves left to play.
-function isMovesLeft(board)
-{
-	for(let i = 0; i < 3; i++)
-		for(let j = 0; j < 3; j++)
-			if (board[i][j] == '_')
+function isMovesLeft(board) {
+	for (let i = 0; i < 3; i++)
+		for (let j = 0; j < 3; j++)
+			if (board[i][j] == '_' || board[i][j] == null)
 				return true;
-				
+
 	return false;
 }
 
-function evaluate(b)
-{
-	
+function evaluate(b) {
+
 	// Checking for Rows for X or O victory.
-	for(let row = 0; row < 3; row++)
-	{
+	for (let row = 0; row < 3; row++) {
 		if (b[row][0] == b[row][1] &&
-			b[row][1] == b[row][2])
-		{
+			b[row][1] == b[row][2]) {
 			if (b[row][0] == player)
 				return +10;
-				
+
 			else if (b[row][0] == opponent)
 				return -10;
 		}
 	}
 
 	// Checking for Columns for X or O victory.
-	for(let col = 0; col < 3; col++)
-	{
+	for (let col = 0; col < 3; col++) {
 		if (b[0][col] == b[1][col] &&
-			b[1][col] == b[2][col])
-		{
+			b[1][col] == b[2][col]) {
 			if (b[0][col] == player)
 				return +10;
 
@@ -55,21 +47,19 @@ function evaluate(b)
 	}
 
 	// Checking for Diagonals for X or O victory.
-	if (b[0][0] == b[1][1] && b[1][1] == b[2][2])
-	{
+	if (b[0][0] == b[1][1] && b[1][1] == b[2][2]) {
 		if (b[0][0] == player)
 			return +10;
-			
+
 		else if (b[0][0] == opponent)
 			return -10;
 	}
 
 	if (b[0][2] == b[1][1] &&
-		b[1][1] == b[2][0])
-	{
+		b[1][1] == b[2][0]) {
 		if (b[0][2] == player)
 			return +10;
-			
+
 		else if (b[0][2] == opponent)
 			return -10;
 	}
@@ -83,8 +73,7 @@ function evaluate(b)
 // considers all the possible ways
 // the game can go and returns the
 // value of the board
-function minimax(board, depth, isMax)
-{
+function minimax(board, depth, isMax) {
 	let score = evaluate(board);
 
 	// If Maximizer has won the game
@@ -103,27 +92,23 @@ function minimax(board, depth, isMax)
 		return 0;
 
 	// If this maximizer's move
-	if (isMax)
-	{
+	if (isMax) {
 		let best = -1000;
 
 		// Traverse all cells
-		for(let i = 0; i < 3; i++)
-		{
-			for(let j = 0; j < 3; j++)
-			{
-				
+		for (let i = 0; i < 3; i++) {
+			for (let j = 0; j < 3; j++) {
+
 				// Check if cell is empty
-				if (board[i][j]=='_')
-				{
-					
+				if (board[i][j] == '_') {
+
 					// Make the move
 					board[i][j] = player;
 
 					// Call minimax recursively
 					// and choose the maximum value
 					best = Math.max(best, minimax(board,
-									depth + 1, !isMax));
+						depth + 1, !isMax));
 
 					// Undo the move
 					board[i][j] = '_';
@@ -134,27 +119,23 @@ function minimax(board, depth, isMax)
 	}
 
 	// If this minimizer's move
-	else
-	{
+	else {
 		let best = 1000;
 
 		// Traverse all cells
-		for(let i = 0; i < 3; i++)
-		{
-			for(let j = 0; j < 3; j++)
-			{
-				
+		for (let i = 0; i < 3; i++) {
+			for (let j = 0; j < 3; j++) {
+
 				// Check if cell is empty
-				if (board[i][j] == '_')
-				{
-					
+				if (board[i][j] == '_') {
+
 					// Make the move
 					board[i][j] = opponent;
 
 					// Call minimax recursively and
 					// choose the minimum value
 					best = Math.min(best, minimax(board,
-									depth + 1, !isMax));
+						depth + 1, !isMax));
 
 					// Undo the move
 					board[i][j] = '_';
@@ -167,8 +148,7 @@ function minimax(board, depth, isMax)
 
 // This will return the best possible
 // move for the player
-function findBestMove(board)
-{
+function findBestMove(board) {
 	let bestVal = -1000;
 	let bestMove = new Move();
 	bestMove.row = -1;
@@ -178,15 +158,12 @@ function findBestMove(board)
 	// minimax function for all empty
 	// cells. And return the cell
 	// with optimal value.
-	for(let i = 0; i < 3; i++)
-	{
-		for(let j = 0; j < 3; j++)
-		{
-			
+	for (let i = 0; i < 3; i++) {
+		for (let j = 0; j < 3; j++) {
+
 			// Check if cell is empty
-			if (board[i][j] == '_')
-			{
-				
+			if (board[i][j] == '_') {
+
 				// Make the move
 				board[i][j] = player;
 
@@ -200,8 +177,7 @@ function findBestMove(board)
 				// If the value of the current move
 				// is more than the best value, then
 				// update best
-				if (moveVal > bestVal)
-				{
+				if (moveVal > bestVal) {
 					bestMove.row = i;
 					bestMove.col = j;
 					bestVal = moveVal;

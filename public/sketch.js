2efc42226a7fbb51a369014f8539de77b63044a9
shreyasborole <shreyas.borole2000@gmail.com>
2022-05-22 12:49:24 +0530

    Added lobby multiplayer support

--- public/sketch.js
+++ public/sketch.js
@@ -2,28 +2,112 @@ let p5sketch = (sketch) => {
     let game;
     let socket;
 
+    let symbol;
+    let myTurn = true;
+
+    let status;
+    let lobbyCode;
+
     sketch.setup = () => {
         sketch.createCanvas(400, 400);
         sketch.frameRate(60);
-        socket = io.connect('http://localhost:3000');
-        socket.on('position', (data) => game.nextTurn(data.posx, data.posy));
+
+        status = sketch.createP('');
+        status.style('font-size', '26pt');
+        lobbyCode = sketch.createP('');
+        lobbyCode.style('font-size', '26pt');
+        lobbyCode.html(code);
+
         game = new TicTacToe(sketch, player);
+
+        socket = io.connect('http://localhost:3000');
+
+        socket.emit('game.code', { host: isHost, lobbyCode: code });
+
+        socket.on('game.invalid.code', () => {
+            lobbyCode.html('Invalid code');
+        });
+
+        // Event is called when either player makes a move
+        socket.on("move.made", (data) => {
+            // Render the move
+            game.nextTurn(data.position.x, data.position.y);
+
+            // If the symbol is the same as the player's symbol,
+            // we can assume it is their turn
+            myTurn = data.symbol !== symbol;
+
+            if (!game.gameOver) renderTurnMessage();
+
+            // // If the game is still going, show who's turn it is
+            // if (!isGameOver()) {
+            //     renderTurnMessage();
+
+            //     // If the game is over
+            // } else {
+            //     // Show the message for the loser
+            //     if (myTurn) {
+            //         status.html('Game Over! You lost');
+            //     } else {
+            //     // Show the message for the winner
+            //         status.html('Game Over! You won');
+            //     }
+
+            // Disable the board
+            // $(".board button").attr("disabled", true);
+        }
+        );
+
+        // Set up the initial state when the game begins
+        socket.on("game.begin", (data) => {
+            // The server will assign X or O to the player
+            symbol = data.symbol;
+
+            // Give X the first turn
+            myTurn = symbol === "X";
+            renderTurnMessage();
+        });
+
+        // Disable the board if the opponent leaves
+        socket.on("opponent.left", () => {
+            status.html('Your opponent left the game');
+        });
+
+    }
+
+    makeMove = (pos) => {
+        // Emit the move to the server
+        socket.emit("make.move", {
+            symbol: symbol,
+            position: pos
+        });
+    }
+
+    renderTurnMessage = () => {
+        // Disable the board if it is the opponents turn
+        if (!myTurn) {
+            status.html("Your opponent's turn");
+            // Enable the board if it is your turn
+        } else {
+            status.html("Your turn");
+        }
     }
 
     sketch.mousePressed = () => {
+        if (!myTurn) return;
         let i = Math.floor(sketch.mouseX / (sketch.width / game.gridSize));
         let j = Math.floor(sketch.mouseY / (sketch.width / game.gridSize));
         if (i >= game.gridSize || j >= game.gridSize || i < 0 || j < 0) return;
-        if(game.checkIfEmpty(i, j)){
-            if(game.currentPlayer != player) return;
-            socket.emit('position', { posx: i, posy: j });
-            game.nextTurn(i, j);
+        if (game.checkIfEmpty(i, j)) {
+            // if (game.currentPlayer != player) return;
+            // socket.emit('position', { posx: i, posy: j });
+            // game.nextTurn(i, j);
+            makeMove({ x: i, y: j });
         }
     }
 
     sketch.draw = () => {
         sketch.background(68, 55, 55);
-
         game.draw();
 
         if (!game.gameOver) {
--- server.js
+++ server.js
@@ -1,27 +1,130 @@
-const http = require('http')
-const express = require('express')
+const http = require('http');
+const express = require('express');
 
 const port = 3000;
 
-const app = express()
-app.use(express.static('public'))
+const app = express();
+app.use(express.static('public'));
 
-app.set('port', `${port}`)
+app.set('port', `${port}`);
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 server.on('listening', () => {
     console.log(`Listening on: http://localhost:${port}`)
-})
+});
 
-server.listen('3000')
+server.listen('3000');
 
-const io = require('socket.io')(server)
-io.sockets.on('connection', (socket) => {
-    console.log(`Client connected id(${socket.id})`);
-    // socket.on('mouse', (data) => socket.broadcast.emit('mouse', data));
-    socket.on('position', (data) => {
-        console.log(data);
-        socket.broadcast.emit('position', data);
+const io = require('socket.io')(server);
+
+const clients = {};
+const lobbies = [];
+
+const addClient = (socket) => {
+    console.log("New client connected", socket.id);
+    clients[socket.id] = socket;
+};
+const removeClient = (socket) => {
+    console.log("Client disconnected", socket.id);
+    delete clients[socket.id];
+};
+
+var players = {},
+    unmatched;
+
+function joinGame(socket) {
+    // Add the player to our object of players
+    players[socket.id] = {
+        // The opponent will either be the socket that is
+        // currently unmatched, or it will be null if no
+        // players are unmatched
+        opponent: unmatched,
+
+        // The symbol will become 'O' if the player is unmatched
+        symbol: "X",
+
+        // The socket that is associated with this player
+        socket: socket,
+
+        // Lobby code created by the player (if any)
+        lobby: null
+    };
+
+    // Every other player is marked as 'unmatched', which means
+    // there is not another player to pair them with yet. As soon
+    // as the next socket joins, the unmatched player is paired with
+    // the new socket and the unmatched variable is set back to null
+    if (unmatched) {
+        players[socket.id].symbol = "O";
+        players[unmatched].opponent = socket.id;
+        unmatched = null;
+    } else {
+        unmatched = socket.id;
+    }
+}
+
+// Returns the opponent socket
+function getOpponent(socket) {
+    try {
+        if (!players[socket.id].opponent) {
+            return;
+        }
+    } catch (error) {
+        return;
+    }
+
+    return players[players[socket.id].opponent].socket;
+}
+
+io.on("connection", (socket) => {
+    socket.on('game.code', (data) => {
+        addClient(socket);
+        if (data.host) {
+            lobbies.push(data.lobbyCode);
+        }
+        else {
+            if(lobbies.indexOf(data.lobbyCode) == -1){
+                socket.emit('game.invalid.code', {});
+                return;
+            }else{
+
+            }
+        }
+        joinGame(socket);
+        // Once the socket has an opponent, we can begin the game
+        if (getOpponent(socket)) {
+            let code = 'lol';
+            socket.emit("game.begin", {
+                symbol: players[socket.id].symbol,
+                lobby: code
+            });
+
+            getOpponent(socket).emit("game.begin", {
+                symbol: players[getOpponent(socket).id].symbol,
+                lobby: code
+            });
+        }
+
+        console.log(lobbies);
     });
-    socket.on('disconnect', () => console.log(`Client disconnected id(${socket.id})`))
+    
+    
+
+    // Listens for a move to be made and emits an event to both
+    // players after the move is completed
+    socket.on("make.move", (data) => {
+        if (!getOpponent(socket)) {
+            return;
+        }
+
+        socket.emit("move.made", data);
+        getOpponent(socket).emit("move.made", data);
+    });
+
+    // Emit an event to the opponent when the player leaves
+    socket.on("disconnect", () => {
+        if (getOpponent(socket)) {
+            getOpponent(socket).emit("opponent.left");
+        }
+    });
 });
